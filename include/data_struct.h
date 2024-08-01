1c3335ef5078655509a804d6e3c3ea4288deee6d
Nicola Rubini <nicola.rubini@cern.ch>
2024-08-01 10:02:28 +0200

    Re-organising and code formatting

--- include/data_struct.h
+++ include/data_struct.h
@@ -4,114 +4,102 @@
 #include <chrono>
 #include <ctime>
 
-
-class data_struct{
+class data_struct
+{
 
 public:
-    data_struct(){
-    }//const
-    ~data_struct(){
-    
-    }//dest
-   void dump_to_csv(std::string output_file_name, std:: vector<pair<std::string, std::string>> data_structure, std::vector<pair<std::string, std::vector<std::pair<std::string, std::string>>>> data_dump = {});
-    
-     void read_csv(const char* filename);
+    data_struct()
+    {
+    } // const
+    ~data_struct()
+    {
 
-    
+    } // dest
+    void dump_to_csv(std::string output_file_name, std::vector<pair<std::string, std::string>> data_structure, std::vector<pair<std::string, std::vector<std::pair<std::string, std::string>>>> data_dump = {});
+
+    void read_csv(const char *filename);
+
 private:
-    float xpos=3.0;
-
-
-
+    float xpos = 3.0;
 };
 
-
-
-void data_struct::dump_to_csv(std::string output_file_name, std:: vector<pair<std::string, std::string>> data_structure, std::vector<pair<std::string, std::vector<std::pair<std::string, std::string>>>> data_dump = {})
+void data_struct::dump_to_csv(std::string output_file_name, std::vector<pair<std::string, std::string>> data_structure, std::vector<pair<std::string, std::vector<std::pair<std::string, std::string>>>> data_dump = {})
 {
-  
-    
-  //  Open output stream
-  std::ofstream output_file(output_file_name);
 
-    
-    
-    
-    
-    
-  //  Time stamp on creation
-  std::time_t current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    //  Open output stream
+    std::ofstream output_file(output_file_name);
 
-  
-    
-    
-  //  Vector of lines, start with creation timestamp
-  std::vector<std::string> all_lines = {std::string("# file created on ") + std::string(std::ctime(&current_time))};
-  
-    
-  //  Category line
-  all_lines.push_back("# ");
-  for (auto [category, default_value] : data_structure)
-      all_lines[1] += std::string(category) + std::string(",  ");
-  all_lines[1] = all_lines[1].substr(0, all_lines[1].size() - 3);
-  all_lines[1] += std::string("\n");
-  //   Data line
-  auto current_pos = 1;
-  for (auto [key, data_vect] : data_dump)
-  {
-    //cout<< key<< endl;
-    all_lines.push_back(key + ",  ");
-    current_pos++;
-    // adding default value only if data vector is smaller than data structure size -1 (first is run tag)
-    if(data_structure.size()-1>data_vect.size()){  
-        std::vector <int> nokey; // vector to store the position with no data   
-        //loop over data structure from 1 as zero is run tag
-        for(int dat_i =1; dat_i<data_structure.size();dat_i++){ 
-            bool has_key = false; // bool to see if the data structure is present in data
-           
-            // loop over the data 
-            for(int dat_j =0; dat_j<data_vect.size();dat_j++){
-                 
-                if(std::strcmp(data_vect[dat_j].first.c_str(),data_structure[dat_i].first.c_str())==0) has_key= true; // if found the structure
-          
-            }//dat_j loop
-         
-        if(!has_key){
-            nokey.push_back(dat_i);
-              
-            data_vect.push_back({"",""});
+    //  Time stamp on creation
+    std::time_t current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    //  Vector of lines, start with creation timestamp
+    std::vector<std::string> all_lines = {std::string("# file created on ") + std::string(std::ctime(&current_time))};
+
+    //  Category line
+    all_lines.push_back("# ");
+    for (auto [category, default_value] : data_structure)
+        all_lines[1] += std::string(category) + std::string(",  ");
+    all_lines[1] = all_lines[1].substr(0, all_lines[1].size() - 3);
+    all_lines[1] += std::string("\n");
+    //   Data line
+    auto current_pos = 1;
+    for (auto [key, data_vect] : data_dump)
+    {
+        // cout<< key<< endl;
+        all_lines.push_back(key + ",  ");
+        current_pos++;
+        // adding default value only if data vector is smaller than data structure size -1 (first is run tag)
+        if (data_structure.size() - 1 > data_vect.size())
+        {
+            std::vector<int> nokey; // vector to store the position with no data
+            // loop over data structure from 1 as zero is run tag
+            for (int dat_i = 1; dat_i < data_structure.size(); dat_i++)
+            {
+                bool has_key = false; // bool to see if the data structure is present in data
+
+                // loop over the data
+                for (int dat_j = 0; dat_j < data_vect.size(); dat_j++)
+                {
+
+                    if (std::strcmp(data_vect[dat_j].first.c_str(), data_structure[dat_i].first.c_str()) == 0)
+                        has_key = true; // if found the structure
+
+                } // dat_j loop
+
+                if (!has_key)
+                {
+                    nokey.push_back(dat_i);
+
+                    data_vect.push_back({"", ""});
+                }
+            } // end data loop
+
+            // loop to fill the default value of key from structure if not found in data
+            for (auto key_pos : nokey)
+            {
+                data_vect[key_pos - 1].first = data_structure[key_pos].first;   // key
+                data_vect[key_pos - 1].second = data_structure[key_pos].second; // default value
+
+            } // end data structure position
+
+        } // END if data_structure is greater than data vector provided
+        for (auto [category, value] : data_vect)
+        {
+
+            all_lines[current_pos] += std::string(value) + std::string(",  ");
         }
-    }// end data loop
-   
-        
-        
-    //loop to fill the default value of key from structure if not found in data
-    for(auto key_pos : nokey){
-        data_vect[key_pos-1].first =    data_structure[key_pos].first; //key 
-        data_vect[key_pos-1].second =    data_structure[key_pos].second;  //default value
-        
-    }// end data structure position   
-      
-    } // END if data_structure is greater than data vector provided 
-    for (auto [category, value] : data_vect){
-        
-        
-        all_lines[current_pos] += std::string(value) + std::string(",  ");
-        
-    }
-     
+
         all_lines[current_pos] = all_lines[current_pos].substr(0, all_lines[current_pos].size() - 3);
-        
+
         all_lines[current_pos] += std::string("\n");
-        
-  }
-  //  Populate lines
-  for (auto current_line : all_lines)
-    output_file << current_line;
+    }
+    //  Populate lines
+    for (auto current_line : all_lines)
+        output_file << current_line;
     output_file.close();
-}//end dumb_to_csv
+} // end dumb_to_csv
 
-void read_csv(const char* filename){
-std::cout << filename << endl;
-
+void read_csv(const char *filename)
+{
+    std::cout << filename << endl;
 }
--- include/plotter.h
+++ include/plotter.h
@@ -1,307 +1,274 @@
 #pragma once
 
-class plotter{
+class plotter
+{
 
 public:
-      
-    
-    plotter(){
-    //constructor
-    }//const
-    
-    ~plotter(
+  plotter()
+  {
+    // constructor
+  } // const
+
+  ~plotter(
       //  rad_ delete;
-       // time delete;
-       // nhits delete;
-    ){
-    
-    //destructor
-    
-    }//dest
-    
-    
-    
-    //void setBinWidhth(int bw =1){
-    //   bin_width= bw ;
-   // }
-    //plot the circle
-    void globalBeauty(){
-       // gStyle->SetPalette(kSolar);
-      //  TColor *color = gROOT->GetColor(20);
-       // color->SetRGB(0.4, 0.6, 0.8); // Soft Blue
-        gStyle->SetOptStat(0);
-        //gPad->SetGrid();
-        gStyle->SetLineWidth(3);
+      // time delete;
+      // nhits delete;
+  )
+  {
 
-        gStyle->SetLegendTextSize(0.03);
-        gStyle->SetLegendBorderSize(0); 
-        
-    }
-    
-    void setCanvasPixel(vector <float> canshape={1200,1200}){
-     canvas_x = canshape[0];
-     canvas_y = canshape[1];   
-        
-        
-        
-    }
-    void plot_circle(std::array<float, 3> parameters, int line_color = kBlack, int line_style = kSolid, int line_width = 1)
-        {
-          auto result = new TEllipse(parameters[0], parameters[1], parameters[2]);
-          result->SetFillStyle(0);
-          result->SetLineColor(line_color);
-          result->SetLineStyle(line_style);
-          result->SetLineWidth(line_width);
-          result->DrawEllipse(parameters[0], parameters[1], parameters[2], 0, 0, 360, 0, "same");
-        }
-    
-    
-    virtual void beautify(TH1D* object,int col, int style){    
+    // destructor
+
+  } // dest
+
+  // void setBinWidhth(int bw =1){
+  //    bin_width= bw ;
+  // }
+  // plot the circle
+  void globalBeauty()
+  {
+    // gStyle->SetPalette(kSolar);
+    //  TColor *color = gROOT->GetColor(20);
+    // color->SetRGB(0.4, 0.6, 0.8); // Soft Blue
+    gStyle->SetOptStat(0);
+    // gPad->SetGrid();
+    gStyle->SetLineWidth(3);
+
+    gStyle->SetLegendTextSize(0.03);
+    gStyle->SetLegendBorderSize(0);
+  }
+
+  void setCanvasPixel(vector<float> canshape = {1200, 1200})
+  {
+    canvas_x = canshape[0];
+    canvas_y = canshape[1];
+  }
+  void plot_circle(std::array<float, 3> parameters, int line_color = kBlack, int line_style = kSolid, int line_width = 1)
+  {
+    auto result = new TEllipse(parameters[0], parameters[1], parameters[2]);
+    result->SetFillStyle(0);
+    result->SetLineColor(line_color);
+    result->SetLineStyle(line_style);
+    result->SetLineWidth(line_width);
+    result->DrawEllipse(parameters[0], parameters[1], parameters[2], 0, 0, 360, 0, "same");
+  }
+
+  virtual void beautify(TH1D *object, int col, int style)
+  {
     object->SetLineWidth(3);
     object->SetLineColor(col);
-     object->SetMarkerColor(col);
-    object->SetMarkerSize(2);       
-    //object->SetOptStat(0);
-    object->SetMarkerStyle(style);     
-    
-    }
-    
-    
-    virtual void beautify(TLine* object,int col, int style){    
+    object->SetMarkerColor(col);
+    object->SetMarkerSize(2);
+    // object->SetOptStat(0);
+    object->SetMarkerStyle(style);
+  }
+
+  virtual void beautify(TLine *object, int col, int style)
+  {
     object->SetLineWidth(3);
     object->SetLineColor(col);
-   // object->SetMarkerColor(col);
- //   object->SetMarkerSize(3);     
-    //object->SetOptStat(0);
-    
-   object->SetLineStyle(style);    
-    
-    }
-    
-    virtual void beautify(TArc* object,int col, int style){    
+    // object->SetMarkerColor(col);
+    //   object->SetMarkerSize(3);
+    // object->SetOptStat(0);
+
+    object->SetLineStyle(style);
+  }
+
+  virtual void beautify(TArc *object, int col, int style)
+  {
     object->SetLineWidth(3);
     object->SetLineColor(col);
-   // object->SetMarkerColor(col);
-  //  object->SetMarkerSize(3);    
-    //object->SetOptStat(0);
-    object->SetLineStyle(style);    
-    //object->SetMarkerStyle(style);     
-    
-    }
-    
-    
-    
-    virtual void SetBinWidthX(float bwidth){
-        bin_width_x= bwidth;
-    }
-    
-    virtual void SetBinWidthY(float bwidth){
-        bin_width_y= bwidth;
-    }
-     
-    // to divide the canvas
-    
-    
-    virtual void Set_Divide_Canvas(bool dodivide, int nrow, int ncol){
-         divide_canvas = dodivide;
-         can_row = nrow;
-         can_column = ncol;
-     }
-    
-    
-    
-    TH2D *plot_histo2d(const char* histname, vector <float> x , vector <float> y){
-      //TCanvas * can = new TCanvas("can", "", 1200,1200);
-     
-      if(x.empty() || y.empty()) 
-      {  
-         TH2D* empty2 = new TH2D("empty2","",100,-10,10,100,-10,10); 
-         cout << "size of x  is " << x.size()<< endl;
-         cout << "size of y  is " << y.size()<< endl;
-          
-          return empty2;}
-      // Use std::max_element to find the maximum element
-      auto xmax_ = std::max_element(x.begin(), x.end());
-      auto  xmin_ = std::min_element(x.begin(), x.end());
-      
-      
-      auto ymax_ = std::max_element(y.begin(), y.end());
-      auto  ymin_ = std::min_element(y.begin(), y.end());
-     
-      
-      float xmax= *xmax_ ;
-      float xmin =*xmin_;
-      float ymax =*ymax_;
-      float ymin =*ymin_;  
-      int nbin_x = (int) TMath::Abs(xmax-xmin)/bin_width_x;  
-      int nbin_y  = (int) TMath::Abs(ymax-ymin)/bin_width_y;  
-        
-        
-      TH2D* hist_2 = new TH2D(histname,"",nbin_x,xmin,xmax,nbin_y,ymin,ymax);
-          
-      for(int i=0; i < x.size();i++){
-      
-      hist_2->Fill(x[i],y[i]);
-      }//end loop
-      //can->cd();
-      //hist_2->Draw(option);
-      //can->SaveAs(Form("%s.png",histname));   
-      return hist_2;
-     
-       
-    } // 
-    
-    
-    
-    TH2D *plot_histo2d(const char* histname, vector <pair<float,float>> p){
-      //TCanvas * can = new TCanvas("can", "", 1200,1200);
-       if(p.empty()) 
-      { 
-           TH2D* empty3 = new TH2D("empty3","",100,-10,10,100,-10,10);
-           cout << "size of x  is " << p.size()<< endl;
-           
-           return empty3;
-       
-      }
-        
-       vector<float> x;
-       vector<float> y; 
-        for (auto pt : p ) {
-      
-          x.push_back(pt.first);
-          y.push_back(pt.second);    
-      } 
-        
-      // Use std::max_element to find the maximum element
-      auto xmax_ = std::max_element(x.begin(), x.end());
-      auto  xmin_ = std::min_element(x.begin(), x.end());
-      
-      
-      auto ymax_ = std::max_element(y.begin(), y.end());
-      auto  ymin_ = std::min_element(y.begin(), y.end());
-     
-      
-      float xmax= *xmax_ ;
-      float xmin =*xmin_;
-      float ymax =*ymax_;
-      float ymin =*ymin_;  
-      int nbin_x = (int) TMath::Abs(xmax-xmin)/bin_width_x;  
-      int nbin_y  = (int) TMath::Abs(ymax-ymin)/bin_width_y;  
-        
-        
-      TH2D* hist_2 = new TH2D(histname,"",nbin_x,xmin,xmax,nbin_y,ymin,ymax);
-          
-      for(int i=0; i < x.size();i++){
-      
-      hist_2->Fill(x[i],y[i]);
-      }//end loop
-      //can->cd();
-      //hist_2->Draw(option);
-      //can->SaveAs(Form("%s.png",histname));   
-      return hist_2;
-     
-       
-    } // 
-    
-    
-    
-     TH1D*  plot_histo1d(const char* histname, vector <float> x ){
-     
-        
+    // object->SetMarkerColor(col);
+    //  object->SetMarkerSize(3);
+    // object->SetOptStat(0);
+    object->SetLineStyle(style);
+    // object->SetMarkerStyle(style);
+  }
 
-          if(x.empty()) 
-          { 
-               TH1D* empty4 = new TH1D("empty4","",100,-10,10);
-              cout << "size of x  is " << x.size()<< endl;
+  virtual void SetBinWidthX(float bwidth)
+  {
+    bin_width_x = bwidth;
+  }
 
+  virtual void SetBinWidthY(float bwidth)
+  {
+    bin_width_y = bwidth;
+  }
 
-              return empty4;}
-          // Use std::max_element to find the maximum element
-          auto xmax_ = std::max_element(x.begin(), x.end());
-          auto  xmin_ = std::min_element(x.begin(), x.end());
+  // to divide the canvas
 
+  virtual void Set_Divide_Canvas(bool dodivide, int nrow, int ncol)
+  {
+    divide_canvas = dodivide;
+    can_row = nrow;
+    can_column = ncol;
+  }
 
+  TH2D *plot_histo2d(const char *histname, vector<float> x, vector<float> y)
+  {
+    // TCanvas * can = new TCanvas("can", "", 1200,1200);
 
-          float xmax= *xmax_ ;
-          float xmin =*xmin_;
-          int nbin_x = (int) TMath::Abs(xmax-xmin)/bin_width_x;  
-
-         // cout << "bin_width " <<  bin_width_x << "   " <<nbin_x << endl;
-          TH1D* hist_1 = new TH1D(histname,"",nbin_x,xmin,xmax);
-
-          for(int i=0; i < x.size();i++){
-
-              hist_1->Fill(x[i]);
-          }//end loop
-         
-         return hist_1;    
-    
-    }
-    
-    
-    
-     TH1D*  plot_histo1d(const char* histname, vector <pair<float,float>> x ){
-     
-        
-
-          if(x.empty()) 
-          { 
-               TH1D* empty4 = new TH1D("empty4","",100,-10,10);
-              cout << "size of x  is " << x.size()<< endl;
-
-
-              return empty4;}
-          // Use std::max_element to find the maximum element
-        //  auto xmax_ = std::max_element(x.begin().first, x.end().first);
-         // auto  xmin_ = std::min_element(x.begin().first, x.end().first);
-
-
-
-          float xmax= 100;//*xmax_ ;
-          float xmin =0;//*xmin_;
-          int nbin_x = (int) TMath::Abs(xmax-xmin)/bin_width_x;  
-
-         // cout << "bin_width " <<  bin_width_x << "   " <<nbin_x << endl;
-          TH1D* hist_1 = new TH1D(histname,"",nbin_x,xmin,xmax);
-
-          for(int i=0; i < x.size();i++){
-                cout << "radius_Scan, " << x[i].first  << "   " <<x[i].second << endl;
-              hist_1->Fill(x[i].first,x[i].second);
-          }//end loop
-         
-         return hist_1;    
-    
-    }
-    
-    TCanvas * BuildCanvas(const char* name)
+    if (x.empty() || y.empty())
     {
-        
-        
-    TCanvas * can = new TCanvas(name, "", canvas_x, canvas_y);
-    if(divide_canvas){
-    can->Divide(can_row,can_column);
-   
-    }    
-        
-    
-    
-    return can; 
+      TH2D *empty2 = new TH2D("empty2", "", 100, -10, 10, 100, -10, 10);
+      cout << "size of x  is " << x.size() << endl;
+      cout << "size of y  is " << y.size() << endl;
+
+      return empty2;
     }
-    
-    
-    
-    
-    
+    // Use std::max_element to find the maximum element
+    auto xmax_ = std::max_element(x.begin(), x.end());
+    auto xmin_ = std::min_element(x.begin(), x.end());
+
+    auto ymax_ = std::max_element(y.begin(), y.end());
+    auto ymin_ = std::min_element(y.begin(), y.end());
+
+    float xmax = *xmax_;
+    float xmin = *xmin_;
+    float ymax = *ymax_;
+    float ymin = *ymin_;
+    int nbin_x = (int)TMath::Abs(xmax - xmin) / bin_width_x;
+    int nbin_y = (int)TMath::Abs(ymax - ymin) / bin_width_y;
+
+    TH2D *hist_2 = new TH2D(histname, "", nbin_x, xmin, xmax, nbin_y, ymin, ymax);
+
+    for (int i = 0; i < x.size(); i++)
+    {
+
+      hist_2->Fill(x[i], y[i]);
+    } // end loop
+    // can->cd();
+    // hist_2->Draw(option);
+    // can->SaveAs(Form("%s.png",histname));
+    return hist_2;
+
+  } //
+
+  TH2D *plot_histo2d(const char *histname, vector<pair<float, float>> p)
+  {
+    // TCanvas * can = new TCanvas("can", "", 1200,1200);
+    if (p.empty())
+    {
+      TH2D *empty3 = new TH2D("empty3", "", 100, -10, 10, 100, -10, 10);
+      cout << "size of x  is " << p.size() << endl;
+
+      return empty3;
+    }
+
+    vector<float> x;
+    vector<float> y;
+    for (auto pt : p)
+    {
+
+      x.push_back(pt.first);
+      y.push_back(pt.second);
+    }
+
+    // Use std::max_element to find the maximum element
+    auto xmax_ = std::max_element(x.begin(), x.end());
+    auto xmin_ = std::min_element(x.begin(), x.end());
+
+    auto ymax_ = std::max_element(y.begin(), y.end());
+    auto ymin_ = std::min_element(y.begin(), y.end());
+
+    float xmax = *xmax_;
+    float xmin = *xmin_;
+    float ymax = *ymax_;
+    float ymin = *ymin_;
+    int nbin_x = (int)TMath::Abs(xmax - xmin) / bin_width_x;
+    int nbin_y = (int)TMath::Abs(ymax - ymin) / bin_width_y;
+
+    TH2D *hist_2 = new TH2D(histname, "", nbin_x, xmin, xmax, nbin_y, ymin, ymax);
+
+    for (int i = 0; i < x.size(); i++)
+    {
+
+      hist_2->Fill(x[i], y[i]);
+    } // end loop
+    // can->cd();
+    // hist_2->Draw(option);
+    // can->SaveAs(Form("%s.png",histname));
+    return hist_2;
+
+  } //
+
+  TH1D *plot_histo1d(const char *histname, vector<float> x)
+  {
+
+    if (x.empty())
+    {
+      TH1D *empty4 = new TH1D("empty4", "", 100, -10, 10);
+      cout << "size of x  is " << x.size() << endl;
+
+      return empty4;
+    }
+    // Use std::max_element to find the maximum element
+    auto xmax_ = std::max_element(x.begin(), x.end());
+    auto xmin_ = std::min_element(x.begin(), x.end());
+
+    float xmax = *xmax_;
+    float xmin = *xmin_;
+    int nbin_x = (int)TMath::Abs(xmax - xmin) / bin_width_x;
+
+    // cout << "bin_width " <<  bin_width_x << "   " <<nbin_x << endl;
+    TH1D *hist_1 = new TH1D(histname, "", nbin_x, xmin, xmax);
+
+    for (int i = 0; i < x.size(); i++)
+    {
+
+      hist_1->Fill(x[i]);
+    } // end loop
+
+    return hist_1;
+  }
+
+  TH1D *plot_histo1d(const char *histname, vector<pair<float, float>> x)
+  {
+
+    if (x.empty())
+    {
+      TH1D *empty4 = new TH1D("empty4", "", 100, -10, 10);
+      cout << "size of x  is " << x.size() << endl;
+
+      return empty4;
+    }
+    // Use std::max_element to find the maximum element
+    //  auto xmax_ = std::max_element(x.begin().first, x.end().first);
+    // auto  xmin_ = std::min_element(x.begin().first, x.end().first);
+
+    float xmax = 100; //*xmax_ ;
+    float xmin = 0;   //*xmin_;
+    int nbin_x = (int)TMath::Abs(xmax - xmin) / bin_width_x;
+
+    // cout << "bin_width " <<  bin_width_x << "   " <<nbin_x << endl;
+    TH1D *hist_1 = new TH1D(histname, "", nbin_x, xmin, xmax);
+
+    for (int i = 0; i < x.size(); i++)
+    {
+      cout << "radius_Scan, " << x[i].first << "   " << x[i].second << endl;
+      hist_1->Fill(x[i].first, x[i].second);
+    } // end loop
+
+    return hist_1;
+  }
+
+  TCanvas *BuildCanvas(const char *name)
+  {
+
+    TCanvas *can = new TCanvas(name, "", canvas_x, canvas_y);
+    if (divide_canvas)
+    {
+      can->Divide(can_row, can_column);
+    }
+
+    return can;
+  }
+
 private:
-    
-    int canvas_x =1200;
-    int canvas_y =1200;
-    bool divide_canvas = false;
-    int can_row = 2;
-    int can_column = 2;
-    float bin_width_x =3.0;
-    float bin_width_y =3.0;
-    
-    
-};    
-    
+  int canvas_x = 1200;
+  int canvas_y = 1200;
+  bool divide_canvas = false;
+  int can_row = 2;
+  int can_column = 2;
+  float bin_width_x = 3.0;
+  float bin_width_y = 3.0;
+};
